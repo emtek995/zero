8640eefbee5be9644c3e71e2e8a64a9d4c5b0dca
Edward Way <emtek995@gmail.com>
2023-05-02 13:11:23 -0500

    Add fake and quickcheck to test valid email

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -23,6 +23,25 @@ impl AsRef<str> for SubscriberEmail {
 mod tests {
     use super::SubscriberEmail;
     use claim::assert_err;
+    use fake::Fake;
+    use fake::faker::internet::en::SafeEmail;
+    use rand::{rngs::SmallRng, SeedableRng}; 
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary(_: &mut quickcheck::Gen) -> Self {
+            let mut rng = SmallRng::from_entropy();
+            let email = SafeEmail().fake_with_rng(&mut rng);
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
+    }
 
     #[test]
     fn empty_string_is_rejected() {
